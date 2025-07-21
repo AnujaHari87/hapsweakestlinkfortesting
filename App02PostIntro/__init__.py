1d124ad6b8c3ef81b0e4874d615588129e4e0012
Hariharan <xr4403@kit.edu>
2025-07-21 21:53:04 +0200

    nasatlx change

--- App02PostIntro/__init__.py
+++ App02PostIntro/__init__.py
@@ -74,7 +74,7 @@ def make_field4(label):
 
 def make_field20(label):
     return models.IntegerField(
-        choices=[ 1, 2, 3,4, 5, 6, 7, 8, 9, 10,11,12,13,14,15,16,17,18,19,20],
+        choices=[0, 1, 2, 3,4, 5, 6, 7, 8, 9, 10,11,12,13,14,15,16,17,18,19,20],
         label=label,
         widget=widgets.RadioSelectHorizontal,
     )
